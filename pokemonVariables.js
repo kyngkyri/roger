ccdc89c701fee4b8ed8ac632d5b390f0ae9d4f14
rveraASC4 <rogerdvera@outlook.com>
2017-07-11 12:07:39 -0400

    first commit

---
+++ pokemonVariables.js
@@ -0,0 +1,15 @@
+var exeggutorHP = 95; // This pokemon has 95 HP
+var exeggutorNum = 103; // Pokedex Number
+var exeggutorType = "Psychic and Grass"; // Pokemon's types
+var exeggutorBestMove = "Leaf Storm"; // Pokemon's best move
+var exeggutorLastEvo = true; // Is in final form
+
+// Bonus Part 1
+
+var pokemonAttack = 50; // Pokemon's hit power
+var pokemonDefense = 100; // Pokemon's defense power
+var pokemonSpeed = 20; // Pokemon's speed
+
+// Bonus Part 3
+
+exeggutorHP = exeggutorHP - 40;
---
+++ roger.js
@@ -0,0 +1,1 @@
+console.log("Roger is awesome!");
